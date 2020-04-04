6600acbeeb85e2f4fb54ed7af938799827d41d45
Schow94 <Schow94@gmail.com>
2020-04-03 17:04:48 -0700

    Comment out seeding Function

--- routes/crypto.js
+++ routes/crypto.js
@@ -7,26 +7,26 @@ const CoinbasePro = require('coinbase-pro');
 const publicClient = new CoinbasePro.PublicClient();
 
 // Initial Seeding of dB
-router.get('/seedDB', async (req, res, next) => {
-  try {
-    //Make API call here
-    const products = await publicClient.getProductHistoricRates('BTC-USD', {
-      granularity: 86400,
-    });
+// router.get('/seedDB', async (req, res, next) => {
+//   try {
+//     //Make API call here
+//     const products = await publicClient.getProductHistoricRates('BTC-USD', {
+//       granularity: 86400,
+//     });
 
-    //Once we have the data, add it to the db
-    for (arr of products) {
-      const data = await db.query(
-        'INSERT INTO BTC_historic_rates (time, low, high, open, close, volume) VALUES ($1, $2, $3, $4, $5, $6)',
-        [arr[0], arr[1], arr[2], arr[3], arr[4], arr[5]]
-      );
-    }
+//     //Once we have the data, add it to the db
+//     for (arr of products) {
+//       const data = await db.query(
+//         'INSERT INTO BTC_historic_rates (time, low, high, open, close, volume) VALUES ($1, $2, $3, $4, $5, $6)',
+//         [arr[0], arr[1], arr[2], arr[3], arr[4], arr[5]]
+//       );
+//     }
 
-    return res.json({ result: data });
-  } catch (e) {
-    return next(e);
-  }
-});
+//     return res.json({ result: data });
+//   } catch (e) {
+//     return next(e);
+//   }
+// });
 
 router.get('/getData', async (req, res, next) => {
   try {
