06cc56d5eb2bf65892b22d70576b3c4c2011c4b1
Schow94 <Schow94@gmail.com>
2020-04-04 14:07:02 -0700

    Comment out seed db function

--- routes/litecoin.js
+++ routes/litecoin.js
@@ -9,27 +9,27 @@ const CoinbasePro = require('coinbase-pro');
 const publicClient = new CoinbasePro.PublicClient();
 
 // Initial Seeding of dB
-router.get('/seedDB', async (req, res, next) => {
-  try {
-    //Make API call here
-    const products = await publicClient.getProductHistoricRates('LTC-USD', {
-      granularity: 86400,
-    });
+// router.get('/seedDB', async (req, res, next) => {
+//   try {
+//     //Make API call here
+//     const products = await publicClient.getProductHistoricRates('LTC-USD', {
+//       granularity: 86400,
+//     });
 
-    //Once we have the data, add it to the db
-    for (arr of products) {
-      const data = await db.query(
-        'INSERT INTO LTC_historic_rates (time, low, high, open, close, volume) VALUES ($1, $2, $3, $4, $5, $6)',
-        [arr[0], arr[1], arr[2], arr[3], arr[4], arr[5]]
-      );
-    }
+//     //Once we have the data, add it to the db
+//     for (arr of products) {
+//       const data = await db.query(
+//         'INSERT INTO LTC_historic_rates (time, low, high, open, close, volume) VALUES ($1, $2, $3, $4, $5, $6)',
+//         [arr[0], arr[1], arr[2], arr[3], arr[4], arr[5]]
+//       );
+//     }
 
-    // API returns 300 days
-    return res.json({ result: data });
-  } catch (e) {
-    return next(e);
-  }
-});
+//     // API returns 300 days
+//     return res.json({ result: data });
+//   } catch (e) {
+//     return next(e);
+//   }
+// });
 
 //Automate API calls to once a day
 //I want to make API call everyday at 5AM
